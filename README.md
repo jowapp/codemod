25c5154ec03bfaface5dd4ce7f07a7f5249cc8ce
hymced <hymced@gmail.com>
2024-05-10 12:14:52 +0200

    feat: add transform + CLI + vitest + config

--- README.md
+++ README.md
@@ -9,10 +9,37 @@ npx @jowapp/codemod <transform> <path> [...options]
 ```
 
 - `transform` - name of transform, see available transforms below.
-- `path` - files or directory to transform
+- `path` - files or directory to transform (use quotes otherwise the path(s) will be expanded by your shell, [which may not support globstar](https://medium.com/@jakubsynowiec/you-should-always-quote-your-globs-in-npm-scripts-621887a2a784))
 - use the `--dry` option for a dry-run and use `--print` to print the output for comparison
 
 This will start an interactive wizard, and then run the specified transform.
+
+All jscodeshift's options are supported, and defaults are the same than the one of jscodeshift's CLI.
+
+Note: jscodeshift's option defaults apply only to its CLI, its API has none, except the parser which defaults to `@babel/parser` (see `babel5Compat.js`)
+
+> All options are also passed to the transformer, which means you can supply custom options that are not listed here.
+
+### Recast Options
+
+Options to [recast](https://github.com/benjamn/recast)'s printer can also be provided through jscodeshift's `printOptions` command line argument, as `react-codemod` does it:
+
+```sh
+npx react-codemod <transform> <path> --jscodeshift="--printOptions='{\"quote\":\"double\"}'"
+```
+
+### Unit Testing
+
+Unit tests are based on [jscodeshift's test utility](https://github.com/facebook/jscodeshift?tab=readme-ov-file#unit-testing) with `vitest`.
+
+But here the directory structure is a bit different:
+
+```
+/subfolder/MyTransform.js
+/__tests__/subfolder/MyTransform-test.js
+/__testfixtures__/subfolder/MyTransform.input.js
+/__testfixtures__/subfolder/MyTransform.output.js
+```
 
 ## Included Transforms
 
---
+++ bin/codemod.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+// jowapp-codemod optional-name-of-transform optional/path/to/src [...options]
+
+const path = require('node:path')
+const yargs = require('yargs')
+const { glob } = require('glob')
+const { run: jscodeshift } = require('jscodeshift/src/Runner')
+
+const TRANSFORMERS = [
+  {
+    name: 'Converts functional components default props to default params',
+    value: 'react.func-default-props-to-params',
+  },
+  {
+    name: 'Converts forwardRef to regular prop ref',
+    value: 'react.forward-ref-to-prop',
+  },
+]
+
+;(async () => {
+  const transformer = process.argv[2]
+  const transformerDirectory = path.join(__dirname, '../', 'transforms')
+
+  let transformerPath
+  if (transformer && !TRANSFORMERS.find((x) => x.value === transformer)) {
+    console.error(
+      `${transformer} not found in available transformers, using current working directory default 'transform.js' (if any).`,
+    )
+    transformerPath = path.resolve('transform.js')
+    console.log(transformerPath)
+    return
+  } else {
+    transformerPath = path.join(transformerDirectory, `${path.join(...transformer.split('.'))}.js`)
+  }
+
+  const { _: pathsArgs, printOptions, ...options } = yargs(process.argv.slice(3)).argv
+
+  let paths
+  if (pathsArgs.length > 0) {
+    if (pathsArgs.some((path) => path.includes('*'))) {
+      try {
+        // expand with globstar
+        paths = await glob(pathsArgs, { ignore: 'node_modules/**' })
+      } catch (e) {
+        console.log(e)
+        process.exit(1)
+      }
+    } else {
+      // user provided multiple paths or a glob pattern expanded by its shell...
+      // paths = pathsArgs.map(p => path.resolve(p))
+      paths = pathsArgs
+    }
+  }
+
+  function parsePrintOptions(value) {
+    try {
+      return JSON.parse(value)
+    } catch (_e) {}
+    return value
+  }
+
+  if (printOptions) options.printOptions = parsePrintOptions(printOptions)
+  if (!options.verbose && !(options.verbose === 0)) options.verbose = 1
+  // babel: apply babeljs to the transform file
+  // yargs parser automatically parses "--no-<option>"" flags to `{ <option>: false }`
+  if (!options.babel && options.babel !== false) options.babel = true
+  // other options can be left undefined and will be treated as if set to jscodeshift defaults
+
+  try {
+    await jscodeshift(transformerPath, paths, options)
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+})()
