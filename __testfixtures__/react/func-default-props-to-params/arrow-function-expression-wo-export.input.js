486fded2f61c6d0d8e8a96f2820ea13168940f6e
hymced <hymced@gmail.com>
2024-05-13 11:26:34 +0200

    fix: review

---
+++ __testfixtures__/react/func-default-props-to-params/arrow-function-expression-wo-export.input.js
@@ -0,0 +1,10 @@
+import React, { forwardRef } from 'react'
+
+const MyInput = ({ prop1, prop2, prop3 }, ref) => {
+  return null
+}
+
+MyInput.defaultProps = {
+  prop1: 'foo',
+  prop3: { bar: 'bar' },
+}
