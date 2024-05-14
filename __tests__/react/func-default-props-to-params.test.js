3fde551ca155c66b5b91598d52de686c7b1cff2b
ntltd <nicolas.taltaud@gmail.com>
2024-05-14 10:28:55 +0200

    test: clean react.func-default-props-to-params

--- __tests__/react/func-default-props-to-params.test.js
+++ __tests__/react/func-default-props-to-params.test.js
@@ -18,35 +18,35 @@ const jsTestsNoOutput = ['function-component-wo-default-props']
 describe('react.func-default-props-to-params', () => {
   jsTests.forEach((test) =>
     defineTest(
-      path.join(__dirname, '..'), // jscodeshift/src/testUtils.js utility assumes transform is one level up from __tests__ directory, but because of 'react' subfolder we have to go up 2 levels from this test file
+      path.join(__dirname, '..'),
       'transforms/react/func-default-props-to-params',
       null,
-      `react/func-default-props-to-params/${test}`, // above dirName arg also applies to __testfixtures__
+      `react/func-default-props-to-params/${test}`,
     ),
   )
-})
 
-describe('react.func-default-props-to-params', () => {
-  jsTestsNoOutput.forEach((test) => {
-    const applyTransform = require('jscodeshift/dist/testUtils').applyTransform
-    const transformOptions = {}
-    const fixtureDir = path.join(
-      __dirname,
-      '..',
-      '..',
-      '__testfixtures__',
-      'react',
-      'func-default-props-to-params',
+  describe('transforms/react/func-default-props-to-params', () => {
+    it.each(jsTestsNoOutput)(
+      'does not transform using "react/func-default-props-to-params/%s"',
+      (test) => {
+        const applyTransform = require('jscodeshift/dist/testUtils').applyTransform
+        const transformOptions = {}
+        const fixtureDir = path.join(
+          __dirname,
+          '..',
+          '..',
+          '__testfixtures__',
+          'react',
+          'func-default-props-to-params',
+        )
+        const inputPath = path.join(fixtureDir, test + '.input.js')
+        const inputSource = fs.readFileSync(inputPath, 'utf8')
+        const output = applyTransform(transform, transformOptions, {
+          source: inputSource,
+          path: inputPath,
+        })
+        expect(output).toBe('')
+      },
     )
-    const inputPath = path.join(fixtureDir, test + '.input.js')
-    const inputSource = fs.readFileSync(inputPath, 'utf8')
-    const output = applyTransform(transform, transformOptions, {
-      source: inputSource,
-      path: inputPath,
-    })
-    it('should be empty string (null transform output)', () => {
-      // applyTransform returns (output || '').trim()
-      expect(output).toBe('')
-    })
   })
 })
